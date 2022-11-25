d92e3bfbac59a90eb34880f749e1b5002a0b2467
Sumanta Das <sumantad827@gmail.com>
2022-11-25 23:54:07 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+# Check if a number is Positive, Negative, or Zero
+
+userinput = int(input("please enter a number : "))
+
+if userinput > 0:
+    print(userinput, "is a Positive number")
+elif userinput < 0:
+    print(userinput, 'is a Negetive number')
+elif userinput == 0:
+    print(userinput, 'is a zero value')
